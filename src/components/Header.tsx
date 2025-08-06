190da7e1610bd58d1403aad897bd54afda26b8c6
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-06 15:49:26 +0100

    Feat: header navigation component

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import { SignInButton, SignUpButton, SignedOut } from "@clerk/nextjs";
+import LoggedInUserNav from "./LoggedInUserNav";
+
+export default async function Header() {
+  const { userId } = await auth();
+  return (
+    <header>
+      <nav className="flex justify-between">
+        <Link href={"/"}>
+          <p>Job Tracker</p>
+        </Link>
+        <div className="flex justify-between gap-4">
+          <SignedOut>
+            <SignInButton mode="modal" />
+            <SignUpButton mode="modal" />
+          </SignedOut>
+          {userId ? <LoggedInUserNav /> : null}
+        </div>
+      </nav>
+    </header>
+  );
+}
