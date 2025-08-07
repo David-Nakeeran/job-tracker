cdb40fedf3e41896fd14628fda36e41893d6ab4d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-07 13:55:24 +0100

    Chore: updated import

--- src/components/LoggedInUserNav.tsx
+++ src/components/LoggedInUserNav.tsx
@@ -1,7 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import DesktopMenu from "./DesktopMenu";
+
 export default function LoggedInUserNav() {
   return (
     <>
-      <h1>Test</h1>
+      <div className="hidden lg:flex gap-4">
+        <DesktopMenu />
+        <UserButton />
+      </div>
     </>
   );
 }
