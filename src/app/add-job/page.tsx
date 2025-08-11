784df57d522d21b5d03a6301e8a555c7614ea935
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-11 16:52:25 +0100

    Update: added userid check

--- src/app/add-job/page.tsx
+++ src/app/add-job/page.tsx
@@ -1,8 +1,14 @@
 import AddJob from "@/components/AddJob";
 import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function AddJobPage() {
   const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+
   return (
     <>
       <AddJob userId={userId} />
