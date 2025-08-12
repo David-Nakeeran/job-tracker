3b8e05627699419e805f581e4d6dccae637cc0c9
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-12 16:17:12 +0100

    Chore: db user querys

---
+++ src/lib/users.ts
@@ -0,0 +1,25 @@
+"use server";
+import { UserIdProps } from "@/types/dataTypes";
+import { db } from "@/utils/dbConnection";
+
+export async function userExists(userId: string) {
+  try {
+    const result = await db.query(`SELECT * FROM jbt_users WHERE id = $1`, [
+      userId,
+    ]);
+    if (result.rows.length === 0) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function insertUser(userId: string) {
+  try {
+    await db.query(`INSERT INTO jbt_users (id) VALUES($1)`, [userId]);
+  } catch (error) {
+    throw error;
+  }
+}
