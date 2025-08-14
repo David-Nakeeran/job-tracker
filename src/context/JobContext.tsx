1660962bfdd6f53d015db339f2d9b934bf057459
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-14 17:08:48 +0100

    Chore: added Job provider

---
+++ src/context/JobContext.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Job, JobProviderProps } from "@/types/dataTypes";
+import { createContext, use, useContext } from "react";
+
+const JobContext = createContext<Job[]>([]);
+// JobContext will always provide a value shaped like Job[] (an array of jobs)
+
+export function JobProvider({ children, jobs }: JobProviderProps) {
+  return <JobContext.Provider value={jobs}>{children}</JobContext.Provider>;
+}
+
+export function useJobs() {
+  return useContext(JobContext);
+}
