9fbfcb35ff4dd51f63ec7f657bc63d78897aaaab
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-15 15:35:14 +0100

    Chore: basic stucture in place with data from props

--- src/components/StatusCard.tsx
+++ src/components/StatusCard.tsx
@@ -1,7 +1,16 @@
-export default function StatusCard() {
+import { StatusCardProps } from "@/types/dataTypes";
+
+export default function StatusCard({
+  status,
+  count,
+  children,
+}: StatusCardProps) {
   return (
     <>
       <h1>StatusCard</h1>
+      <h2>{status}</h2>
+      <p>{count}</p>
+      {children}
     </>
   );
 }
