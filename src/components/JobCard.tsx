29e43a837c928402807efd3941c65fd7db3800c8
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-18 15:55:11 +0100

    Updated: added basic component structure

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,7 +1,30 @@
-export default function JobCard() {
+import { JobCardProps } from "@/types/dataTypes";
+
+export default function JobCard({
+  position,
+  company,
+  status,
+  date_applied,
+  location,
+  notes,
+  work_type,
+  salary,
+  job_url,
+  description,
+}: JobCardProps) {
   return (
     <>
       <h1>JobCard</h1>
+      <p>{position}</p>
+      <p>{company}</p>
+      <p>Status: {status}</p>
+      <p>Applied: {date_applied}</p>
+      {location && <p>{location}</p>}
+      {salary && <p>{salary}</p>}
+      {job_url && <a href={job_url}>Job Link</a>}
+      {description && <p>{description}</p>}
+      {notes && <p>{notes}</p>}
+      <p>Type: {work_type}</p>
     </>
   );
 }
