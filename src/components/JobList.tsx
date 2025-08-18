2d1f1997fc329983e83250a1dbeb67bd1a8e1208
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-18 15:55:41 +0100

    Update: passed props to component

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -3,9 +3,28 @@ import JobCard from "./JobCard";
 
 export default function JobList({ jobs }: JobProps) {
   //loop through jobs arr and pass props to JobCards
+  const jobElements = jobs.map((job) => {
+    const date = job.date_applied.toLocaleDateString("en-GB");
+    return (
+      <JobCard
+        key={job.id}
+        position={job.position}
+        company={job.company}
+        status={job.status}
+        date_applied={date}
+        location={job.location}
+        notes={job.notes}
+        work_type={job.work_type}
+        salary={job.salary}
+        job_url={job.job_url}
+        description={job.description}
+      />
+    );
+  });
   return (
     <>
       <h1>JobLists</h1>
+      {jobElements}
     </>
   );
 }
