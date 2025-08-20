e345fbc526260f2c7b62367e49dc83c66f699874
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-20 17:14:01 +0100

    Feat: delete component

---
+++ src/components/DeleteAlertDialog.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { JobIdProps } from "@/types/dataTypes";
+import { deleteJob } from "@/lib/actions";
+
+export function DeleteAlertDialog({ jobId }: JobIdProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className="text-red-500 hover:text-red-400 border border-red-500 hover:bg-[#2a2a2c]"
+        >
+          Delete Job
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-red-500">
+            Are you sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete this job.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel className="bg-[#2a2a2c] text-[#e8e8e8] border border-[#3d3d3f] hover:bg-[#3d3d3f] hover:text-[#e8e8e8]">
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction
+            className="bg-red-500 text-white hover:bg-red-700"
+            onClick={() => deleteJob(jobId)}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
