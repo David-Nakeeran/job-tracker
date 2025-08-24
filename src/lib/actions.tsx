4ecaab477cd8111aff726fba5803f12625db6ffd
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-24 11:39:25 +0100

    Update: added new server action

--- src/lib/actions.tsx
+++ src/lib/actions.tsx
@@ -52,7 +52,48 @@ export async function createJob(id: string, values: JobFormValues) {
   }
 }
 
-export async function deleteJob(jobId: string) {
+export async function updateJob(jobId: number, values: JobFormValues) {
+  // Server validation
+  const result = jobSchema.safeParse(values);
+
+  if (!result.success) {
+    const { fieldErrors, formErrors } = z.flattenError(result.error);
+
+    const errorMessages = [...Object.values(fieldErrors).flat(), ...formErrors];
+
+    return { success: false as const, errorMessages };
+  }
+
+  try {
+    await db.query(
+      `UPDATE jbt_jobs SET position = $1, company = $2, date_applied = $3, location = $4, status = $5, notes = $6, work_type = $7, salary = $8, job_url = $9, description = $10 WHERE id = $11`,
+      [
+        result.data.position,
+        result.data.company,
+        result.data.date_applied,
+        result.data.location,
+        result.data.status,
+        result.data.notes,
+        result.data.work_type,
+        result.data.salary,
+        result.data.job_url,
+        result.data.description,
+        jobId,
+      ]
+    );
+    revalidatePath("/dashboard");
+    return { success: true as const, message: "Job updated successfully!" };
+  } catch (error) {
+    let errorMessage = "Error updating job";
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    console.error(errorMessage);
+    return { success: false as const, errorMessage: [errorMessage] };
+  }
+}
+
+export async function deleteJob(jobId: number) {
   await db.query(
     `
     DELETE FROM jbt_jobs WHERE id = $1`,
