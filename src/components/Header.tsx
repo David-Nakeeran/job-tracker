790a8538f4468e1b6417fbd339c7be797f7e0f59
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-08-25 15:35:17 +0100

    Feat: added margin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,17 @@ import LoggedInUserNav from "./LoggedInUserNav";
 export default async function Header() {
   const { userId } = await auth();
   return (
-    <header>
+    <header className="mb-6">
       <nav className="flex justify-between">
         <Link href={"/"}>
-          <p>Job Tracker</p>
+          <p className="text-[#ce2772]">Job Tracker</p>
         </Link>
         <div className="flex justify-between gap-4">
           <SignedOut>
             <SignInButton mode="modal" />
             <SignUpButton mode="modal" />
           </SignedOut>
-          {userId ? <LoggedInUserNav /> : null}
+          {userId && <LoggedInUserNav />}
         </div>
       </nav>
     </header>
